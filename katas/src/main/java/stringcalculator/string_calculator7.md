217d0b5374284da63117e7596e0f7cf211edf3f0
dwhelan <dwhelan@leanintuit.com>
2016-09-30 21:23:15 -0700

    Added solution to Roman Numeral Calculator.

--- katas/src/main/java/stringcalculator/string_calculator7.md
+++ katas/src/main/java/stringcalculator/string_calculator7.md
@@ -2,4 +2,6 @@
 
 Delimiters can be of any length with the following format:  `//[delimiter]\n` for example: `//***\n1***2***3` should return `6`.
 
-Allow multiple delimiters like: `//[delim1][delim2]\n` for example `//[*][%]\n1*2%3` should return `6`.
+Allow multiple delimiters like: `//[delim1][delim2]\n`.
+
+For example, `//[*][%]\n1*2%3` should return `6`.
---
+++ katas/src/test/solution/romannumeralcalculator/RomanNumeralCalculator2Test.java
@@ -0,0 +1,46 @@
+package romannumeralcalculator;
+
+import static java.text.MessageFormat.format;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RomanNumeralCalculator2Test {
+	private int input;
+	private String expected;
+
+	@Parameters
+	public static Collection<Object[]> testData() {
+		return asList(new Object[][] {
+			{ 1, "I" },
+		});
+	}
+
+	public RomanNumeralCalculator2Test(int input, String expected) {
+		super();
+		this.input = input;
+		this.expected = expected;
+	}
+
+	@Test
+	public void transformed_input_should_equal_expected() {
+		assertThat(message(), process(), is(expected));
+	}
+
+	private String message() {
+		return format("Should convert {0} to {1}", input, expected);
+	}
+
+	private String process() {
+		return RomanNumeralCalculator2.convert(input);
+	}
+}
